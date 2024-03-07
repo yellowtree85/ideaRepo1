16b00e00a388f61b37dc91a98d3a854e026d5e9e
yellowtree85 <tianxiayangjilong@163.com>
2024-03-07 16:00:13 +0800

    modify DateTest.java

--- src/com/xinyu/DateTest.java
+++ src/com/xinyu/DateTest.java
@@ -7,8 +7,11 @@ import java.util.Date;
 public class DateTest {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
+       Date date = new Date(calendar.getTimeInMillis()) ;
         Date now = calendar.getTime();
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(sf.format(now));
+        System.out.println("Date1: "+sf.format(now));
+        System.out.println("Date2: "+sf.format(date));
+
     }
 }
